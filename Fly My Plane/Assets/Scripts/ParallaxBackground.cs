bef232502d22de3652a5aa128847eb9f715fd7bf
Gusti Zainokta <zainokta@ymail.com>
2018-09-24 10:38:42 +0700

    add new project

---
+++ Fly My Plane/Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour {
+    private float ySpeed = 0.3f;
+    public Material mat;
+    Vector2 offset = Vector2.zero;
+    // Use this for initialization
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.y += ySpeed * Time.deltaTime;
+        if (offset.y > 0.1f)
+            offset.y -= 0.9f;
+        else if (offset.y < -0.1f)
+            offset.y += 0.9f;
+        mat.mainTextureOffset = offset;
+    }
+}
---
+++ Fly My Plane/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Move(5 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Move(-5 * Time.deltaTime);
+        }
+
+        if (Input.acceleration.x != 0)
+        {
+            Move(Input.acceleration.x / 5);
+        }
+    }
+
+    void Move(float speed)
+    {
+        if (speed > 0.5f)
+        {
+            speed = 0.5f;
+        }
+        
+        if (speed < -0.5f)
+        {
+            speed = -0.5f;
+        }
+
+        if (Mathf.Abs(transform.position.x) <= 2f)
+            Debug.Log("MWATEK");
+            //transform.Translate(speed, 0, 0);
+    }
+}
