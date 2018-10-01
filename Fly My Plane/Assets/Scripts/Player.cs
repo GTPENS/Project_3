397ac3ebecef1337d033c63f0293147693d9bd3f
Gusti Zainokta <zainokta@ymail.com>
2018-10-01 09:42:26 +0700

    add explosion

--- Fly My Plane/Assets/Scripts/Player.cs
+++ Fly My Plane/Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     
+    public GameObject explosion;
+
 	void Update () {
         if (Input.GetKey(KeyCode.D))
         {
@@ -32,7 +34,19 @@ public class Player : MonoBehaviour {
             speed = -0.5f;
         }
 
-        if (Mathf.Abs(transform.position.x) <= 2f)
+        if (Mathf.Abs(transform.position.x) <= 2.5f)
+        {
             transform.Translate(speed, 0, 0);
+            //health ngurang
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Asteroid")
+        {
+            Destroy(collision.gameObject);
+            GameObject explosionGO = Instantiate(explosion, transform.position = new Vector2(transform.position.x, transform.position.y + 1), Quaternion.identity);
+        }
     }
 }
